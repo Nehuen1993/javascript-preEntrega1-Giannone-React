2d945710e82f18ddc4002a37c07d91ad75dc79bd
Nehuen Giannone <n.giannone@hotmail.com>
2023-05-25 11:33:56 -0300

    correcciones

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,16 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
+import logo from './imagenes/logo.png';
+
 
 
 const NavBar = () => {
   return (
     <nav className="navbar bg-body-tertiary">
       <form className="container-fluid justify-content-start">
-        <button className="btn btn-outline-success me-2" type="button" href="#Button1">Button1</button>
-        <button className="btn btn-outline-success me-2" type="button" href="#Button2">Button2</button>
-        <button className="btn btn-outline-success me-2" type="button" href="#Button3">Button3</button>
-        <button className="btn btn-outline-success me-2" type="button" href="#Button4">Button4</button>
+        
+        <img src={logo} height="40px" width="40px"></img>
+        <button className="btn btn-outline-success me-2" type="button" href="#trenDelantero">Tren Delantero</button>
+        <button className="btn btn-outline-success me-2" type="button" href="#motor">Motor</button>
+        <button className="btn btn-outline-success me-2" type="button" href="#frenos">Frenos</button>
+        <button className="btn btn-outline-success me-2" type="button" href="#inyeccion">Inyeccion</button>
         <CartWidget />
       </form>
     
